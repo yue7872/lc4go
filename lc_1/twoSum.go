8dd781527a38fb09d69cf377b8514834c5db051b
yue7872 <2258462872@qq.com>
2022-10-24 17:45:23 +0800

    feat: two sum

--- lc_1/twoSum.go
+++ lc_1/twoSum.go
@@ -1,5 +1,58 @@
 package lc_1
 
+// // 双重循环，判断i和j相加是否等于target
+// func Answer(nums []int, target int) []int {
+// 	for i := 0; i < len(nums); i++ {
+// 		for j := i + 1; j < len(nums); j++ {
+// 			if nums[i]+nums[j] == target {
+// 				s := []int{i, j}
+// 				return s
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
+
+// 单循环 判断是否有nums[]中是否存在target-nums[i]
+// go中没有indexOf之类的属性 不好实现
+
+// map实现
+// 将i, nums[i]存入key中，得到myMap
+
+// myMap[nums[i]] = i;
+// 判断 myMap[target - nums[i]]是否存在
+
+// myMap[i] = nums[i]
+// 判断 myMap中是否有target-num[i]
+// 但这样就失去了map的意义，和数组一样
+
+// func Answer(nums []int, target int) []int {
+// 	myMap := make(map[int]int)
+// 	for i := 0; i < len(nums); i++ {
+// 		myMap[nums[i]] = i
+// 	}
+// 	for i := 0; i < len(nums); i++ {
+// 		if v, s := myMap[target-nums[i]]; s && (v != i) {
+// 			return []int{i, v}
+// 		}
+// 	}
+// 	return nil
+// }
+
+// Map 优化
+// 优化点：
+// 1. 不用先把所有值都存入map，一边查找一边存储即可
+// 这样做好处：减少循环、每次查找完再存可以保证不会重复
+// 2. 用range循环？
+// 3. 不用make创建map?
+// 返回的数据注意先p后k，因为查找的是k之前的，即nums[0] ~ nums[k-1]之间，p<k
 func Answer(nums []int, target int) []int {
-	return nums
+	myMap := map[int]int{}
+	for k, v := range nums {
+		if p, ok := myMap[target-v]; ok {
+			return []int{p, k}
+		}
+		myMap[v] = k
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	input_1 := []int{2, 7, 11, 15}
-	input_2 := 9
+	input_1 := []int{3, 2, 4}
+	input_2 := 6
 	fmt.Print("answer is:", lc_1.Answer(input_1, input_2))
 }
